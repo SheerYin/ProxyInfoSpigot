fb3d17f6a288be3290b0873e00e5b75f5762df25
SheerYin <sheerhuangjinyin@gmail.com>
2024-05-11 16:09:05 +0800

    初始化

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import java.text.SimpleDateFormat
+import java.util.*
+
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+var pluginVersion = SimpleDateFormat("yyyy.MM.dd").format(Date()) + "-SNAPSHOT"
+group = "io.github.yin.proxyinfospigot"
+version = ""
+
+repositories {
+    mavenLocal()
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
+}
+
+tasks.register("writePluginVersion") {
+    doLast {
+        file("src/main/resources/plugin.yml").apply {
+            writeText(readText().replace("{pluginVersion}", pluginVersion))
+        }
+    }
+}
+
+tasks.register("restorePluginVersion") {
+    doLast {
+        file("src/main/resources/plugin.yml").apply {
+            writeText(readText().replace(pluginVersion, "{pluginVersion}"))
+        }
+    }
+}
+
+tasks.named("compileJava") {
+    dependsOn("writePluginVersion")
+}
+
+tasks.named("build") {
+    finalizedBy("restorePluginVersion")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/io/github/yin/proxyinfospigot/ProxyInfoSpigotMain.kt
@@ -0,0 +1,166 @@
+package io.github.yin.proxyinfospigot
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabExecutor
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.plugin.messaging.PluginMessageListener
+import java.io.ByteArrayInputStream
+import java.io.DataInputStream
+
+
+class ProxyInfoSpigotMain : JavaPlugin(), TabExecutor, PluginMessageListener {
+
+    companion object {
+        lateinit var instance: ProxyInfoSpigotMain
+        const val prefix = "§f[§3代理信息§f] "
+        const val pluginChannel = "proxyinfo:channel"
+        var serverName = ""
+        var players = listOf<String>()
+    }
+
+    override fun onEnable() {
+        instance = this
+        server.consoleSender.sendMessage(prefix + "插件开始加载 " + description.version)
+
+        getCommand("proxyinfospigot")?.setExecutor(this)
+        server.messenger.registerOutgoingPluginChannel(this, pluginChannel)
+        server.messenger.registerIncomingPluginChannel(this, pluginChannel, this)
+    }
+
+    override fun onDisable() {
+        server.consoleSender.sendMessage(prefix + "插件开始卸载 " + description.version)
+    }
+
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        arguments: Array<out String>
+    ): Boolean {
+        when (arguments.size) {
+            0 -> {}
+            1 -> {
+                when {
+                    suggestion(arguments[0], "servername", sender) -> {
+                        sender.sendMessage(prefix + "在代理的服务器名是 " + serverName)
+                    }
+                    suggestion(arguments[0], "players", sender) -> {
+                        sender.sendMessage(prefix + "代理玩家列表 " + players)
+                    }
+                }
+            }
+        }
+        return true
+    }
+
+    override fun onTabComplete(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        arguments: Array<out String>
+    ): List<String> {
+        return when (arguments.size) {
+            1 -> {
+                listMatches(arguments[0], listOf("servername", "players"))
+            }
+
+            else -> {
+                emptyList()
+            }
+        }
+    }
+
+    private fun listMatches(argument: String, suggest: Iterable<String>): MutableList<String> {
+        return suggest.filter { it.contains(argument) }.toMutableList()
+    }
+
+    private fun permissionMessage(sender: CommandSender, permission: String): Boolean {
+        if (sender.hasPermission(permission)) {
+            return true
+        }
+        sender.sendMessage("${prefix}您没有 $permission 权限")
+        return false
+    }
+
+    private fun suggestion(argument: String, suggest: String, sender: CommandSender): Boolean {
+        if (argument.equals(suggest, ignoreCase = true)) {
+            return permissionMessage(
+                sender,"${description.name.lowercase()}.command.$suggest"
+            )
+        }
+        return false
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    override fun onPluginMessageReceived(channel: String, player: Player, message: ByteArray) {
+        if (channel == pluginChannel) {
+            DataInputStream(ByteArrayInputStream(message)).use { input ->
+                val action = input.readUTF().lowercase()
+                when (action) {
+                    "servername" -> {
+                        serverName = input.readUTF()
+                    }
+                    "players" -> {
+                        players = input.readUTF().split(", ")
+                    }
+                }
+            }
+        }
+    }
+
+
+
+}
+
+
+
+
+
+    /*
+    // 手动模式
+    @EventHandler(priority = EventPriority.NORMAL)
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        if (serverName.isEmpty()) {
+            val byteArrayOutputStream = ByteArrayOutputStream()
+            DataOutputStream(byteArrayOutputStream).use { out ->
+                out.writeUTF("servername")
+            }
+            Bukkit.getOnlinePlayers().first().sendPluginMessage(this, pluginChannel, byteArrayOutputStream.toByteArray())
+            loadPlayers()
+        }
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    override fun onPluginMessageReceived(channel: String, player: Player, message: ByteArray) {
+        if (channel == pluginChannel) {
+            DataInputStream(ByteArrayInputStream(message)).use { input ->
+                val action = input.readUTF()
+                when (action) {
+                    "servername" -> {
+                        serverName = input.readUTF()
+                    }
+                    "players" -> {
+                        players = input.readUTF().split(", ")
+                    }
+                }
+            }
+        }
+    }
+
+    fun loadPlayers() {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        DataOutputStream(byteArrayOutputStream).use { out ->
+            out.writeUTF("players")
+        }
+        Bukkit.getOnlinePlayers().first().sendPluginMessage(this, pluginChannel, byteArrayOutputStream.toByteArray())
+    }
+
+     */
+
+
+
+
